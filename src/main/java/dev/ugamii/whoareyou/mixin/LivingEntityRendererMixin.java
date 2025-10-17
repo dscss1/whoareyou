0a4fef96e24023550fda42625f1545e49b805bde
dscss1 <euser6256@gmail.com>
2025-10-17 13:03:23 +0200

    fix(mixin): update hasLabel injection signature for 1.21.8 mappings

--- src/main/java/dev/ugamii/whoareyou/mixin/LivingEntityRendererMixin.java
+++ src/main/java/dev/ugamii/whoareyou/mixin/LivingEntityRendererMixin.java
@@ -1,9 +1,9 @@
 package dev.ugamii.whoareyou.mixin;
 
+import dev.ugamii.whoareyou.config.WhoAreYouConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.entity.LivingEntity;
-import dev.ugamii.whoareyou.config.WhoAreYouConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,19 +12,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntityRenderer.class)
 public class LivingEntityRendererMixin {
 	@Inject(
+			method = "hasLabel(Lnet/minecraft/entity/LivingEntity;D)Z",
 			at = @At("HEAD"),
-			method = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;hasLabel(Lnet/minecraft/entity/LivingEntity;)Z",
 			cancellable = true
 	)
-	private void viewOwnLabel(LivingEntity ent, CallbackInfoReturnable<Boolean> ci) {
-		if (WhoAreYouConfig.enabledOwnName && ent == MinecraftClient.getInstance().cameraEntity)
-		{
-			ci.setReturnValue(MinecraftClient.isHudEnabled());
-		}
-		if (WhoAreYouConfig.enabledOtherPlayersName && ent != MinecraftClient.getInstance().cameraEntity)
-		{
-			ci.setReturnValue(MinecraftClient.isHudEnabled());
+	private void whoareyou$showNameLabel(LivingEntity entity, double distanceSq, CallbackInfoReturnable<Boolean> cir) {
+		var cameraEntity = MinecraftClient.getInstance().cameraEntity;
+
+		if (entity == null || cameraEntity == null) return;
+
+		if (WhoAreYouConfig.enabledOwnName && entity == cameraEntity) {
+			cir.setReturnValue(MinecraftClient.isHudEnabled());
+		} else if (WhoAreYouConfig.enabledOtherPlayersName && entity != cameraEntity) {
+			cir.setReturnValue(MinecraftClient.isHudEnabled());
 		}
 	}
 }
-
